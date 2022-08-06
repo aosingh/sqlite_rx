ea1c58999b0671e117a2abb85e208a623f11ef39
Abhishek Singh <abhishek.singh@qsensei.com>
2022-08-06 13:32:12 -0700

    PEP 517 compliant build using pyproject.toml

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from os import path
 
 from setuptools import find_packages, setup
-from sqlite_rx import __version__
 
 
 this_directory = path.abspath(path.dirname(__file__))
@@ -9,7 +8,7 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-VERSION = __version__
+VERSION = '1.1.0'
 DISTNAME = 'sqlite_rx'
 LICENSE = 'MIT License'
 AUTHOR = 'Abhishek Singh'
